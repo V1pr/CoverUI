2a158f56815ebbd5bb642f07c342a991ca3ad533
V1pr <viper@vipernet.hu>
2026-03-14 21:58:34 +0100

    last version

--- Firmware/CoverUI/YardForce/model/LegacyHAT/assembly.hpp
+++ Firmware/CoverUI/YardForce/model/LegacyHAT/assembly.hpp
@@ -54,19 +54,15 @@ SW8 - S1
 SW9 - S2
 */
 
-#define BTN_S1_PIN PA15 // SW8 - D10; PA15
-#define BTN_S2_PIN PB3 // SW9 - D11; PB3
-#define BTN_PLAY_PIN PB5 // SW1; PB5
-#ifdef USBD_USE_CDC
-#define BTN_HOME_PIN PB7 // SW4; PA12 // we fake it to other pin, since PA12 is used by CDC
-#else
-#define BTN_HOME_PIN PA12 // SW4; PA12
-#endif
-#define BTN_LOCK_PIN PB4 // SW7 - D9; PB4
+#define BTN_S1_PIN PB5 // SW8 - D10; PA15
+#define BTN_S2_PIN PB4 // SW9 - D11; PB3
+#define BTN_PLAY_PIN PB7 // SW1; PB5
+#define BTN_HOME_PIN PA15 // SW4;
+#define BTN_LOCK_PIN PB6 // SW7 - D9; PB4
 
 #define BTN_4H_PIN PA6 // SW3 - 4H
 #define BTN_6H_PIN PB0 // SW5 - 6H
-#define BTN_8H_PIN PA10 // SW6 - 8H
+#define BTN_8H_PIN PA11 // SW6 - 8H
 #define BTN_10H_PIN PB9 // SW2 - 10H
 
 // STOP button on the outer cover
@@ -74,18 +70,23 @@ SW9 - S2
 #define PIN_ESTOP2 PB13 // ESTOP2 from original CoverUI - JP10 on LegacyHAT - 9 pin connector + LevelShift
 
 // handle with 2 switches on either sidesx
-#define PIN_HANDLE_BTN_1 PA8 // Handle Button1 - on the LegacyHAT, PB15 if on OEM-CoverUI
+#define PIN_HANDLE_BTN_1 PA9 // Handle Button1 - on the LegacyHAT, PB15 if on OEM-CoverUI
 #ifndef USBD_USE_CDC // usb CDC debug uses PA11
-#define PIN_HANDLE_BTN_2 PA11 // Handle Button2 - on the LegacyHAT, always routed to PA11
+#define PIN_HANDLE_BTN_2 PA8 // Handle Button2 - on the LegacyHAT, always routed to PA11
 #endif
 
-#define PIN_HALL1 PB6 // LiftX - JP1 pin 10 on OEM CoverUI
-#define PIN_HALL2 PA9 // Left bumper - JP1 pin 7 on OEM CoverUI
+#define PIN_HALL1 PA10 // LiftX - JP1 pin 10 on OEM CoverUI
+#ifdef USBD_USE_CDC
+#define PIN_HALL2 PB15 // Left bumper - JP1 pin 7 on OEM CoverUI // we fake it to other pin, since PA12 is used by CDC
+#else
+#define PIN_HALL2 PA12 // Left bumper - JP1 pin 7 on OEM CoverUI
+#endif
+
 #define PIN_HALL3 PB14 // Lift - JP1 pin 4 on OEM CoverUI
-#define PIN_HALL4 PB13 // Right bumper - JP1 pin 1 on OEM CoverUI
+#define PIN_HALL4 PB15 // Right bumper - JP1 pin 1 on OEM CoverUI
 
 // checkme
-#define PIN_RAIN PB1             
+#define PIN_RAIN PA1             
 #define RAIN_ADC_THRESHOLD 700   // Why a threshold? Cause it could be made configurable on (Stock-)CoverUI (if i.e. required due to inaccuracy)
 #define RAIN_PROCESS_PERIOD 5000 // c.ez proposed "once a second or every 10 seconds"
 
